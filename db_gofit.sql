6e42744e14cfda0969df1d9ae497fdd62d9f7917
farras13 <farrasgopal13@gmail.com>
2020-11-11 20:10:54 +0700

    update db

--- db_gofit.sql
+++ db_gofit.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2020 at 06:16 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.3.21
+-- Generation Time: Nov 11, 2020 at 02:10 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,26 +25,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detailfood`
---
-
-CREATE TABLE `detailfood` (
-  `idDetailFood` int(11) NOT NULL,
-  `idFood` int(11) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `food`
 --
 
 CREATE TABLE `food` (
   `idFood` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `image` text NOT NULL
+  `image` text NOT NULL,
+  `portion` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  `calories` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`idFood`, `name`, `image`, `portion`, `time`, `calories`) VALUES
+(1, 'adadad', 'adadad', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingridients`
+--
+
+CREATE TABLE `ingridients` (
+  `idIngridients` int(11) NOT NULL,
+  `idFood` int(11) NOT NULL,
+  `ingridients` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ingridients`
+--
+
+INSERT INTO `ingridients` (`idIngridients`, `idFood`, `ingridients`) VALUES
+(1, 1, 'asdasdsad'),
+(2, 1, 'qeqeqwe'),
+(3, 1, 'wqrqtq');
 
 -- --------------------------------------------------------
 
@@ -84,18 +104,21 @@ CREATE TABLE `listsubworkout` (
   `image` text NOT NULL,
   `set` int(11) NOT NULL,
   `duration` int(11) NOT NULL,
-  `rest` int(11) NOT NULL
+  `rest` int(11) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `listsubworkout`
 --
 
-INSERT INTO `listsubworkout` (`idWorkoutDetail`, `idWorkout`, `name`, `image`, `set`, `duration`, `rest`) VALUES
-(1, 1, 'gopal', '/asset/au/popo.png', 2, 3, 10),
-(2, 2, 'dosa', '/asset/su/popo.png', 3, 4, 10),
-(3, 1, 'nada', '/asset/au/popo.png', 4, 5, 2),
-(4, 2, 'yori', '/asset/su/opo.png', 5, 4, 5);
+INSERT INTO `listsubworkout` (`idWorkoutDetail`, `idWorkout`, `name`, `image`, `set`, `duration`, `rest`, `link`, `start`, `end`) VALUES
+(1, 1, 'gopal', '/asset/au/popo.png', 2, 3, 10, '', 0, 0),
+(2, 2, 'dosa', '/asset/su/popo.png', 3, 4, 10, '', 0, 0),
+(3, 1, 'nada', '/asset/au/popo.png', 4, 5, 2, '', 0, 0),
+(4, 2, 'yori', '/asset/su/opo.png', 5, 4, 5, '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -110,19 +133,40 @@ CREATE TABLE `listworkout` (
   `type` varchar(50) NOT NULL,
   `total` int(11) NOT NULL,
   `duration` int(11) NOT NULL,
-  `rounds` int(11) NOT NULL
+  `rounds` int(11) NOT NULL,
+  `link` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `listworkout`
 --
 
-INSERT INTO `listworkout` (`idWorkout`, `image`, `name`, `type`, `total`, `duration`, `rounds`) VALUES
-(1, '/asset/asu/popo.png', 'halo', 'lala', 20000, 23, 2),
-(2, '/asset/asu/pop.png', 'plias', 'tsa', 230000, 21, 2),
-(3, 'http://res.cloudinary.com/gofit2/image/upload/v1604919828/txhtjpeud4zg3wlsjkzy.jpg', 'bismillah', '1', 2, 3, 4),
-(4, '', 'bismillah', '1', 2, 3, 4),
-(5, 'http://res.cloudinary.com/gofit2/image/upload/v1604920189/pq8uxlamoguas3spq8vd.jpg', 'bismillah', '12', 23, 32, 41);
+INSERT INTO `listworkout` (`idWorkout`, `image`, `name`, `type`, `total`, `duration`, `rounds`, `link`) VALUES
+(1, '/asset/asu/popo.png', 'halo', 'lala', 20000, 23, 2, ''),
+(2, '/asset/asu/pop.png', 'plias', 'tsa', 230000, 21, 2, ''),
+(3, 'http://res.cloudinary.com/gofit2/image/upload/v1604919828/txhtjpeud4zg3wlsjkzy.jpg', 'bismillah', '1', 2, 3, 4, ''),
+(4, '', 'bismillah', '1', 2, 3, 4, ''),
+(5, 'http://res.cloudinary.com/gofit2/image/upload/v1604920189/pq8uxlamoguas3spq8vd.jpg', 'bismillah', '12', 23, 32, 41, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `methods`
+--
+
+CREATE TABLE `methods` (
+  `idMethods` int(11) NOT NULL,
+  `idFood` int(11) NOT NULL,
+  `methods` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `methods`
+--
+
+INSERT INTO `methods` (`idMethods`, `idFood`, `methods`) VALUES
+(1, 1, 'asdasdasd'),
+(2, 1, 'sadasdasdv');
 
 -- --------------------------------------------------------
 
@@ -175,17 +219,17 @@ INSERT INTO `user` (`idUser`, `username`, `password`, `name`, `date`, `gender`, 
 --
 
 --
--- Indexes for table `detailfood`
---
-ALTER TABLE `detailfood`
-  ADD PRIMARY KEY (`idDetailFood`),
-  ADD KEY `idFood` (`idFood`);
-
---
 -- Indexes for table `food`
 --
 ALTER TABLE `food`
   ADD PRIMARY KEY (`idFood`);
+
+--
+-- Indexes for table `ingridients`
+--
+ALTER TABLE `ingridients`
+  ADD PRIMARY KEY (`idIngridients`),
+  ADD KEY `idFood` (`idFood`);
 
 --
 -- Indexes for table `listsubmuscle`
@@ -208,6 +252,13 @@ ALTER TABLE `listworkout`
   ADD PRIMARY KEY (`idWorkout`);
 
 --
+-- Indexes for table `methods`
+--
+ALTER TABLE `methods`
+  ADD PRIMARY KEY (`idMethods`),
+  ADD KEY `idFood` (`idFood`);
+
+--
 -- Indexes for table `muscle`
 --
 ALTER TABLE `muscle`
@@ -224,16 +275,16 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `detailfood`
---
-ALTER TABLE `detailfood`
-  MODIFY `idDetailFood` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `idFood` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idFood` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ingridients`
+--
+ALTER TABLE `ingridients`
+  MODIFY `idIngridients` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `listsubmuscle`
@@ -254,6 +305,12 @@ ALTER TABLE `listworkout`
   MODIFY `idWorkout` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `methods`
+--
+ALTER TABLE `methods`
+  MODIFY `idMethods` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `muscle`
 --
 ALTER TABLE `muscle`
@@ -270,10 +327,10 @@ ALTER TABLE `user`
 --
 
 --
--- Constraints for table `detailfood`
+-- Constraints for table `ingridients`
 --
-ALTER TABLE `detailfood`
-  ADD CONSTRAINT `detailfood_ibfk_1` FOREIGN KEY (`idFood`) REFERENCES `food` (`idFood`);
+ALTER TABLE `ingridients`
+  ADD CONSTRAINT `ingridients_ibfk_1` FOREIGN KEY (`idFood`) REFERENCES `food` (`idFood`);
 
 --
 -- Constraints for table `listsubmuscle`
@@ -286,6 +343,12 @@ ALTER TABLE `listsubmuscle`
 --
 ALTER TABLE `listsubworkout`
   ADD CONSTRAINT `listsubworkout_ibfk_1` FOREIGN KEY (`idWorkout`) REFERENCES `listworkout` (`idWorkout`);
+
+--
+-- Constraints for table `methods`
+--
+ALTER TABLE `methods`
+  ADD CONSTRAINT `methods_ibfk_1` FOREIGN KEY (`idFood`) REFERENCES `food` (`idFood`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
